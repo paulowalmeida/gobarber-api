7f63520ebae39f6bc7d3bf9778fb5636eb51d3df
Paulo W. A. Ferreira <pauloweskley.dev@gmail.com>
2020-06-11 17:15:27 -0300

    - Configuração da tabela appointment no Migrations; - Ajustes nos demais arquivos de configuração como ormconfig.json, tsconfig.json e .eslintrc.json; - Mapeamento da classe Appointment.ts com decorators;

--- src/database/migrations/1591885597849-CreateAppointments.ts
+++ src/database/migrations/1591885597849-CreateAppointments.ts
@@ -1,11 +1,35 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
 export class CreateAppointments1591885597849 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'appointment',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'varchar',
+                        isPrimary: true,
+                        generationStrategy: 'uuid'
+                    },
+                    {
+                        name: 'provider',
+                        type: 'varchar',
+                        isNullable: false
+                    },
+                    {
+                        name: 'date',
+                        type: 'datetime',
+                        isNullable: false
+                    }
+                ]
+            })
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('appointment');
     }
 
 }
--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,15 +1,27 @@
 import {uuid} from "uuidv4";
+import {Column, Entity, PrimaryGeneratedColumn} from "typeorm";
 
+@Entity('appointment')
 class Appointment {
+
+    @PrimaryGeneratedColumn("uuid")
     id?: string;
+
+    @Column()
     provider: string;
+
+    @Column("datetime")
     date: Date;
 
+    /*
+    * Será gerado automaticamente por causa do mapeamento do TypeORM
+    *
     constructor({provider, date}: Omit<Appointment, 'id'>) {
         this.id = uuid();
         this.provider = provider;
         this.date = date;
     }
+     */
 }
 
 export default Appointment;
