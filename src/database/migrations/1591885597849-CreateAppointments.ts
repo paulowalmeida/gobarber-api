96043b7874861f63208e5244ae8d5b658de78e87
Paulo W. A. Ferreira <pauloweskley.dev@gmail.com>
2020-06-11 11:42:54 -0300

    - Configuração do TypeOrm; - Configuração inicial do Migrations;

---
+++ src/database/migrations/1591885597849-CreateAppointments.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateAppointments1591885597849 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
