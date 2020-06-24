fbf75d92d98a2247581bacb188a0347dbe4d2297
Paulo W. A. Ferreira <pauloweskley.dev@gmail.com>
2020-06-24 03:28:12 -0300

    Upload de imagem para avatar.

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -3,27 +3,39 @@ import {getCustomRepository} from "typeorm";
 import UsersRepository from "../repositories/UsersRepository";
 import {hash} from 'bcryptjs';
 import CreateUserService from "../services/CreateUserService";
-
+import ensureAuthenticated from "../middlewares/ensureAuthenticated";
+import multer from "multer";
+import uploadConfig from "../config/upload";
 
 const usesRouter = Router();
+const upload = multer(uploadConfig);
 
-usesRouter.get('/', async (req, res) => {
+usesRouter.get('/', async (request, response) => {
     const repository = getCustomRepository(UsersRepository);
     const users = await repository.find();
-    return res.json(users);
+    return response.json(users);
 });
 
-usesRouter.post('/', async (req, res) => {
+usesRouter.post('/', async (request, response) => {
     try {
-        const {name, email, password} = req.body;
+        const {name, email, password} = request.body;
         const service = new CreateUserService();
         const hashedPassword = await hash(password, 8);
         const user = await service.execute({name, email, password: hashedPassword});
         delete user?.password;
-        return res.json(user);
+        return response.json(user);
     } catch (e) {
-        return res.status(400).json({error: e.message});
+        return response.status(400).json({error: e.message});
     }
 });
+
+usesRouter.patch(
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    async (request, response) => {
+        console.log(request.file);
+        return response.json({message: 'ok'})
+    });
 
 export default usesRouter;
